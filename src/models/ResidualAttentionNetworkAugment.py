70cecfe62cb9d319348d1e7c1ba3813d41cf5dff
thecobb <jacobnnye@gmail.com>
2021-12-02 12:57:35 -0500

    Added cifar10 example notebook and functions for loading data using tf.data
    Minor optimization implemented for training, reduces training step time for a batch of 256 from 136 ms/step to 112ms/step on a P100 google collab pro instance. This reduces training time by 3 seconds per epoch.

---
+++ src/models/ResidualAttentionNetworkAugment.py
@@ -0,0 +1,95 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from .residual_unit import ResidualUnit
+from .attention_module import AttentionModule
+
+
+class ResidualAttentionNetwork(keras.Model):
+    """
+    Implementation of Residual Attention Network using predefined Residual and Attention Blocks
+    """
+    
+    def __init__(self, input_shape=(32,32,3), num_class=10, channels=64, p=1, t=2, r=1, **kwargs):
+        
+        """
+        :params:
+        1. input_shape -> 3 elements tuple (height, width, channel) of input image
+        2. num_class -> number of output class
+        3. channels -> number of channel for 
+        4. p -> number of preprocessing residual units in each stage
+        5. t -> number of residual units in the trunk branch
+        6. r -> number of residual units between adjacent pooling layer in the soft mask branch
+        """
+        
+        super(ResidualAttentionNetwork, self).__init__(**kwargs)
+
+        self.p = p
+        self.t = t
+        self.r = r
+        
+        ### Initialize layers needed
+
+        self.conv1 = layers.Conv2D(filters=32, input_shape=input_shape, kernel_size=3, strides=1, padding='same', use_bias=False)
+        self.batch_norm1 = layers.BatchNormalization()
+        self.conv1_activation = layers.ReLU()
+        
+        self.residual_unit1 = ResidualUnit(channels=128, strides=2)
+        self.attention_module1 = AttentionModule(channels=128, stage=2, p=self.p, t=self.t, r=self.r)
+        
+        self.residual_unit2 = ResidualUnit(channels=256, strides=2)
+        self.attention_module2 = AttentionModule(channels=256, stage=3, p=self.p, t=self.t, r=self.r)
+        
+        self.residual_unit3 = ResidualUnit(channels=512, strides=2)
+        self.residual_unit4 = ResidualUnit(channels=512)
+        self.residual_unit5 = ResidualUnit(channels=512)
+        
+        self.batch_norm2 = layers.BatchNormalization()
+        self.activation = layers.ReLU()
+        self.avgpool = layers.AveragePooling2D(pool_size=4, strides=1)
+            
+        # fully connected layers
+        self.flatten = layers.Flatten()
+        self.fc1 = layers.Dense(channels//2, activation='relu')
+        self.fc2 = layers.Dense(num_class, activation='softmax')
+
+        #data augmentation layers
+         # Create a data augmentation stage with horizontal flipping, rotations, zooms
+          # Create a data augmentation stage with horizontal flipping, rotations, zooms
+        self.data_augmentation = keras.Sequential(
+            [
+                layers.experimental.preprocessing.RandomFlip(mode="horizontal"),
+                layers.experimental.preprocessing.RandomRotation(0.2),
+                layers.experimental.preprocessing.RandomTranslation(height_factor=0.2, width_factor=0.2),
+            ]
+        )
+
+    def call(self, x):
+        """
+        Forward pass for the network.
+        """
+        x = self.data_augmentation(x)
+        x = self.conv1(x)
+        x = self.batch_norm1(x)
+        x = self.conv1_activation(x)
+
+        x = self.residual_unit1(x)
+        x = self.attention_module1(x)
+
+        x = self.residual_unit2(x)
+        x = self.attention_module2(x)
+
+        x = self.residual_unit3(x)
+        x = self.residual_unit4(x)
+        x = self.residual_unit5(x)
+
+        x = self.batch_norm2(x)
+        x = self.activation(x)
+        x = self.avgpool(x)
+
+        # top layer
+        x = self.flatten(x) 
+        x = self.fc1(x)
+        x = self.fc2(x)
+
+        return x
--- src/utils/generate_data.py
+++ src/utils/generate_data.py
@@ -1,7 +1,15 @@
 import numpy as np
+from sklearn.utils import shuffle
 import tensorflow as tf
 import tensorflow.keras as keras
 import tensorflow.keras.preprocessing as preprocessing
+from tensorflow.keras import layers
+from tensorflow.data import Dataset
+import os
+import json
+import pprint
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 def normalize(x):
     x = x.astype("float32")
@@ -38,6 +46,41 @@ def preprocess_dataset(x_train, y_train, x_test, y_test):
 
     return x_train, y_train, x_test, y_test
 
+
+def preprocess_dataset_tensor(x_train, y_train, x_test, y_test):
+    '''
+    1. Normalize images
+    2. Set label as one-hot encoded vector
+    3. Subtract pixel mean
+    
+    :input_param:
+    x_train, y_train, x_test, y_test
+    
+    :return:
+    preprocessed x_train, y_train, x_test, y_test
+    '''
+
+    # normalize pixel value
+    x_train = normalize(x_train)
+    x_test = normalize(x_test)
+
+    # label one hot encoding
+    y_train = keras.utils.to_categorical(y_train)
+    y_test = keras.utils.to_categorical(y_test)
+
+    # mean centering
+    x_train = x_train - x_train.mean()
+    x_test = x_test - x_test.mean()
+
+    x_train = x_train.astype(np.float32)
+    x_test = x_test.astype(np.float32)
+
+    #create validation data from training data
+    x_train, x_val, y_train, y_val = train_test_split(x_train,y_train,test_size=0.20,stratify=y_train,random_state=42,shuffle=True)
+
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+
 def get_cifar10(**kwargs):
     '''
     Load CIFAR data with preprocessing and image datagen
@@ -62,4 +105,10 @@ def get_cifar10(**kwargs):
     datagen = preprocessing.image.ImageDataGenerator(**kwargs)
     datagen.fit(x_train)
 
-    return x_train, y_train, x_test, y_test, datagen
+    return x_train, y_train, x_test, y_test, datagen    #test - 20% of total dataset
+    test_dataset = Dataset.from_tensor_slices((x_test, y_test))
+    #shuffle, batch, and prefetch data
+    test_dataset = test_dataset.shuffle(buffer_size=shuffle_buffer).batch(batch_size).prefetch(buffer_size=autotune)
+
+
+    return train_dataset, validation_dataset, test_dataset
